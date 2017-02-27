488b84244e3bc5847f94c768a4b7d995715af607
Jasper Hou <jasper.hou@bmw.com>
2017-02-27 10:39:42 +0800

    Setup different build types and flavors.

--- AndroidLab/app/src/main/java/com/example/androidlab/MainActivity.java
+++ AndroidLab/app/src/main/java/com/example/androidlab/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.androidlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello = (TextView)findViewById(R.id.hello);
+        hello.setText(R.string.PARKING_SEARCH_KEY_WORD);
     }
 }
